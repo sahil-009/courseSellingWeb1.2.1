aa2415f54faca8080b97e7b7bcda2bd3fa851ec5
sahil-009 <sahilmund.6685@gmail.com>
2024-10-20 19:01:41 +0530

    adminWorkingon-postMan-DB collection s not showing data!!

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const { Router } = require('express');
 const  route = Router();
+const jwt = require('jsonwebtoken');
+const { adminModel } = require('../db');
+const JWT_ADMIN_PASSWORD = "userpassword";
 
 
- route.post("/signup", function(req, res) {
+ route.post("/signup", async function(req, res) {
+    const { email, firstName, lastName, password } = req.body;
+
+    await adminModel.create({
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: password
+    });
     res.json({
         message: "User signed up successfully"
     });
 });
 
- route.post("/signin", function(req, res) {
+ route.post("/signin", async function(req, res) {
+
+    const { email, password } = req.body;
+    const admin = await adminModel.findOne({
+         email: email,
+          password: password //122345
+    });
+
+    // Token based authentication
+    if (admin){
+        const token = jwt.sign({ 
+            id: admin._id,
+         }, JWT_ADMIN_PASSWORD);
+        return res.json({
+            message: "User signed in successfully",
+            token: token
+        });
+    }
+    else{
+        res.status(403).json({
+            message: "Invalid email or password"
+        });
+    }
     res.json({
         message: "User signed in successfully"
     });
